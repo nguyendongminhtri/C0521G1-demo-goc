5e92eb38cb907c5869c3e0d07d5b4a8b9d8f8d8b
nguyendongminhtri <73800938+nguyendongminhtri@users.noreply.github.com>
2021-10-22 10:49:19 +0700

    update

---
+++ src/main/java/com/example/demo/service/ICategoryService.java
@@ -0,0 +1,15 @@
+package com.example.demo.service;
+
+import com.example.demo.model.Category;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.Optional;
+
+public interface ICategoryService {
+    Page<Category> findAll(Pageable pageable);
+    Optional<Category> findById(Long id);
+    void deleteById(Long id);
+    Category save(Category category);
+    boolean existsByNameCategory(String nameCategory);
+}
